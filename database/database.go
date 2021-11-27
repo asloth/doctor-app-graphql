f8e1640287f55a18e795f1fbac2bc781d44277df
Sara <41219424+asloth@users.noreply.github.com>
2021-11-27 04:18:26 -0500

    se cambio de prisma a gorm exitosamente

---
+++ database/database.go
@@ -0,0 +1,97 @@
+package database
+
+import (
+	"time"
+)
+
+// https://github.com/jackc/pgx
+
+type UserType struct {
+	ID    uint `gorm:"primaryKey;autoIncrement"`
+	Name  string
+	Users []User
+}
+
+type User struct {
+	ID         uint   `gorm:"primaryKey;autoIncrement"`
+	Name       string `gorm:"not null"`
+	Password   string `gorm:"not null"`
+	UserTypeID uint
+	UserType   UserType `gorm:"foreignKey:UserTypeID;references:ID"`
+}
+
+type Patient struct {
+	ID                       uint `gorm:"primaryKey;autoIncrement"`
+	UserID                   uint
+	User                     User
+	ClinicHistory            ClinicHistory
+	DocumentId               string
+	Name                     string
+	Lastname                 string
+	Email                    string
+	PhoneNumber              string
+	SisId                    string
+	Genre                    string
+	Birthdate                time.Time
+	Address                  string
+	District                 string
+	Region                   string
+	IdentificationDocumentID uint
+	IdentificationDocument   IdentificationDocument `gorm:"foreignKey:IdentificationDocumentID;references:ID"`
+	DocumentIdUrl            string
+	Attentions               []Attention
+}
+
+type Doctor struct {
+	ID         uint `gorm:"primaryKey;autoIncrement"`
+	UserID     uint
+	User       User `gorm:"foreignKey:UserID;references:ID"`
+	Name       string
+	Lastname   string
+	DocumentId string
+	Email      string
+	Schedule   []DoctorSchedule
+	Attentions []Attention
+}
+
+type DoctorSchedule struct {
+	ID        uint `gorm:"primaryKey;autoIncrement"`
+	InitDate  time.Time
+	EndDate   time.Time
+	StartHour uint8 `gorm:"check:start_hour<24;check:start_hour>0"`
+	EndHour   uint8 `gorm:"check:end_hour<24;check:end_hour>0"`
+	DoctorID  uint
+}
+
+type IdentificationDocument struct {
+	ID       uint `gorm:"primaryKey;autoIncrement"`
+	Name     string
+	Patients []Patient
+}
+
+type ClinicHistory struct {
+	ID          uint `gorm:"primaryKey;autoIncrement"`
+	Name        string
+	DocumentUrl string
+	PatientID   uint
+}
+
+type MedicalCenter struct {
+	ID         uint `gorm:"primaryKey;autoIncrement"`
+	Name       string
+	Address    string
+	OpenHour   uint8 `gorm:"check:open_hour<24;check:open_hour>0"`
+	CloseHour  uint8 `gorm:"check:close_hour<24;check:close_hour>0"`
+	Attentions []Attention
+}
+
+type Attention struct {
+	ID              uint `gorm:"primaryKey;autoIncrement"`
+	DoctorID        uint
+	Doctor          Doctor `gorm:"foreignKey:DoctorID;references:ID"`
+	PatientID       uint
+	Patient         Patient `gorm:"foreignKey:PatientID;references:ID"`
+	MedicalCenterID uint
+	MedicalCenter   MedicalCenter `gorm:"foreignKey:MedicalCenterID;references:ID"`
+	Date            time.Time
+}
--- server.go
+++ server.go
@@ -7,13 +7,37 @@ import (
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/asloth/doctor-app-graphql/database"
 	"github.com/asloth/doctor-app-graphql/graph"
 	"github.com/asloth/doctor-app-graphql/graph/generated"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 const defaultPort = "8080"
 
 func main() {
+
+	var dsn = "host=localhost user=postgres password=73481200 dbname=doctorapp-db port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// Migrate the schema
+	db.AutoMigrate(&database.UserType{},
+		&database.User{},
+		&database.Patient{},
+		&database.MedicalCenter{},
+		&database.IdentificationDocument{},
+		&database.Doctor{},
+		&database.DoctorSchedule{},
+		&database.ClinicHistory{},
+		&database.Attention{},
+	)
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
